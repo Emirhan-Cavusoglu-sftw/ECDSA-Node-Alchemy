cfae86466d7ed4efe8324cf87a5da5cc996486f0
Emirhan Cavusoglu <116461155+Emirhan-Cavusoglu-sftw@users.noreply.github.com>
2023-05-04 14:30:18 +0300

    Add files via upload

---
+++ ECDSA-Node/ecdsa-node/client/src/Transfer.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1"
+import { keccak256 } from 'ethereum-cryptography/keccak'
+import { utf8ToBytes, toHex } from 'ethereum-cryptography/utils'
+
+function Transfer({ address, setBalance, privateKey }) {
+  const [sendAmount, setSendAmount] = useState("");
+  const [recipient, setRecipient] = useState("");
+
+  const setValue = (setter) => (evt) => setter(evt.target.value);
+
+  async function transfer(evt) {
+    evt.preventDefault();
+
+    try {
+
+      const tx = {
+        sender: address,
+        amount: parseInt(sendAmount),
+        recipient,
+      }
+
+      //hashing transaction data and create a signature
+      const msgHash = keccak256(utf8ToBytes(JSON.stringify(tx)));
+      const [signature, recoveryBit] = await secp.sign(msgHash, privateKey, { recovered: true });
+
+      //passing data on to server side
+      const {
+        data: { balance, debug },
+      } = await server.post(`send`, {
+        sender: address,
+        amount: parseInt(sendAmount),
+        recipient,
+        msgHash,
+        signature,
+        recoveryBit
+      });
+
+      console.log('server side:', debug);
+      setBalance(balance);
+    } catch (ex) {
+      alert(ex.response.data.message);
+    }
+  }
+
+  return (
+    <form className="container transfer" onSubmit={transfer}>
+      <h1>Send Transaction</h1>
+
+      <label>
+        Send Amount
+        <input
+          placeholder="1, 2, 3..."
+          value={sendAmount}
+          onChange={setValue(setSendAmount)}
+        ></input>
+      </label>
+
+      <label>
+        Recipient
+        <input
+          placeholder="Copy paste an Address, see info section below"
+          value={recipient}
+          onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <input type="submit" className="button" value="Transfer" />
+    </form>
+  );
+}
+
+export default Transfer;
